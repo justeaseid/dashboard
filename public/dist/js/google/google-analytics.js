456664e2051bb674b356b03b198db8d80740887c
paramafadli <parama.fadli@gmail.com>
2018-02-13 13:54:07 +0700

    JA-04 add GTM access

--- public/dist/js/google/google-analytics.js
+++ public/dist/js/google/google-analytics.js
@@ -14,4 +14,4 @@
     j.src =
             'https://www.googletagmanager.com/gtm.js?id=' + i + dl;
     f.parentNode.insertBefore(j, f);
-})(window, document, 'script', 'dataLayer', 'GTM-MFWVLLN');
+})(window, document, 'script', 'dataLayer', 'GTM-WJJBWJ3');
