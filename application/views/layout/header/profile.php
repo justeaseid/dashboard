7314d2a6599639dc409b0d9629ddf8582a4065e1
paramafadli <parama.fadli@gmail.com>
2018-02-16 05:32:40 +0700

    JA-31 update design header profile

--- application/views/layout/header/profile.php
+++ application/views/layout/header/profile.php
@@ -11,15 +11,16 @@
                     <img src="<?php echo USER_ICON; ?>" class="user-image" alt="User Image">
                     <span class="hidden-xs"><?php echo $name; ?></span>
                 </a>
-                <ul class="dropdown-menu" style="width: 200px; height: 80px;">
+                <!--<ul class="dropdown-menu" style="width: 200px; height: 80px;">-->
+                <ul class="dropdown-menu" style="width: 200px; height: 40px;">
                     <li>
                         <!-- inner menu: contains the actual data -->
                         <ul class="menu">
-                            <li>
-                                <a class = "fa fa-user" href="<?php echo base_url('/apps/profile/' . $url. '/' . $id_user); ?>">
+<!--                            <li>
+                                <a class = "fa fa-user" href="<?php // echo base_url('/apps/profile/' . $url. '/' . $id_user); ?>">
                                     &nbsp;&nbsp;Profile
                                 </a>
-                            </li>
+                            </li>-->
                             <li>
                                 <a class = "fa fa-sign-out" href="<?php echo base_url('/apps/logout/' . $email); ?>">
                                     &nbsp;&nbsp;Sign Out
